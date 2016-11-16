1fd79d53a21f049b9df3977c8c24a3e476f210f0
Srinivas92 <srinivasaperumals@tadconsultancy.com>
2016-11-16 14:24:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-// Create connection to Oracle
-$conn = oci_connect("system", "system", "127.0.0.1/xe");
-if (!$conn) {
-   $m = oci_error();
-   echo $m['message'], "\n";
-   exit;
-}
-else {
-   print "Connected to Oracle!";
-}
-// Close the Oracle connection
-oci_close($conn);
+echo "php file";
 ?>
